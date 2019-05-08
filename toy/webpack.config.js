8fb90aa5cb75fb9911923fdbfe1ff72228df347a
Hyungki <hyungkip@cicomprogram.com>
2019-05-08 10:36:59 -0700

    [setup] webpack and config added

---
+++ toy/webpack.config.js
@@ -0,0 +1,8 @@
+﻿const path = require('path');
+module.exports = {
+    entry: './wwwroot/src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'bundle.js'
+    }
+};
