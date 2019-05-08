1e004166206a0a59a7db6c27d3dc30cf4f6da19a
Hyungki <hyungkip@cicomprogram.com>
2019-05-08 16:06:22 -0700

    [feature] add movie page adds movie successfully to local storage

--- toy/wwwroot/src/App.vue
+++ toy/wwwroot/src/App.vue
@@ -8,7 +8,30 @@
         </div>
 
         <div id="homePage" class="mt-4" v-if="home">
-            <kendo-grid class="mt-4" :data-source="localDataSource">
+            <kendo-grid class="mt-4" :data-source="myMovies">
+
+                <kendo-grid-column :field="'movieName'"
+                                   :title="'Movie Name'"
+                                   :width="40"></kendo-grid-column>
+                <kendo-grid-column :field="'monthWatched'"
+                                   :title="'Month watched'"
+                                   :width="40"></kendo-grid-column>
+                <kendo-grid-column :field="'dayWatched'"
+                                   :title="'Day watched'"
+                                   :width="40"></kendo-grid-column>
+                <kendo-grid-column :field="'yearWatched'"
+                                   :title="'Year watched'"
+                                   :width="40""></kendo-grid-column>
+                <kendo-grid-column :field="'genre'"
+                                   :title="'Genre'"
+                                   :width="40"></kendo-grid-column>
+                <kendo-grid-column :field="'rating'"
+                                   :title="'Rating'"
+                                   :width="40"></kendo-grid-column>
+                <kendo-grid-column :field="'comments'"
+                                   :title="'Comments'"
+                                   :width="40"></kendo-grid-column>
+
             </kendo-grid>
         </div>
 
@@ -16,47 +39,50 @@
             <form>
                 <div class="form-group">
                     <label for="movieName"><b>Movie Name</b></label>
-                    <input type="text" class="form-control" id="movieName" placeholder="">
+                    <input type="text" id="movieName" class="form-control" v-model="movieName"/>
                 </div>
                 <div class="form-group">
                     <label><b>Date watched</b></label>
                     <div class="col-md">
                         <label for="monthWatched">Month</label>
-                        <input type="number" class="form-control" id="monthWatched" />
+                        <input type="number" id="monthWatched" class="form-control" min="1" max="12" v-model="monthWatched" />
                     </div>
                     <div class="col-md">
                         <label for="dayWatched">Day</label>
-                        <input type="number" class="form-control" id="dayWatched" />
+                        <input type="number" id="dayWatched" class="form-control" min="1" max="31" v-model="dayWatched" />
                     </div>
-
-
-
-
+                    <div class="col-md">
+                        <label for="yearWatched">Year</label>
+                        <input type="number" id="dayWatched" class="form-control" min="1900" max="2020" v-model="yearWatched" />
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="genre"><b>Genre</b></label>
-                    <select multiple class="form-control" id="genre">
-                        <option>Comedy</option>
-                        <option>Horror</option>
-                        <option>Drama</option>
-                        <option>Sci-fi</option>
-                        <option>Romance</option>
+                    <select class="form-control" id="genre" v-model="genre">
+                        <option value="comedy">Comedy</option>
+                        <option value="horror">Horror</option>
+                        <option value="drama">Drama</option>
+                        <option value="scifi">Sci-fi</option>
+                        <option value="romance">Romance</option>
+                        <option value="thriller">Thriller</option>
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="rating"><b>Rating</b></label>
-                    <select multiple class="form-control" id="rating">
-                        <option>1 (Lame)</option>
-                        <option>2 (Mediocre)</option>
-                        <option>3 (Good)</option>
-                        <option>4 (Amazing)</option>
-                        <option>5 (Best movie ever)</option>
+                    <select class="form-control" id="rating" v-model="rating">
+                        <option value="1">1 (Lame)</option>
+                        <option value="2">2 (Mediocre)</option>
+                        <option value="3">3 (Good)</option>
+                        <option value="4">4 (Amazing)</option>
+                        <option value="5">5 (Best movie ever)</option>
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="comments"><b>Comments</b></label>
-                    <textarea class="form-control" id="comments" rows="3"></textarea>
+                    <textarea class="form-control" id="comments" rows="3" v-model="comments"></textarea>
                 </div>
+
+                <input class="btn btn-success btn-block" type="button" @click="submitForm()" value="Submit"/>
             </form>
         </div>
 
@@ -75,54 +101,31 @@ export default {
         data: function () {
             return {
                 home: true,
+                movieName: '',
+                monthWatched: 1,
+                dayWatched: 1,
+                yearWatched: 2019,
+                genre: '',
+                rating: 0,
+                comments: '',
 
-                localDataSource: [{
-                    "ProductID": 1,
-                    "ProductName": "Chai",
-                    "UnitPrice": 18,
-                    "UnitsInStock": 39,
-                    "Discontinued": false,
-                },
-                {
-                    "ProductID": 2,
-                    "ProductName": "Chang",
-                    "UnitPrice": 17,
-                    "UnitsInStock": 40,
-                    "Discontinued": false,
-                },
-                {
-                    "ProductID": 3,
-                    "ProductName": "Aniseed Syrup",
-                    "UnitPrice": 10,
-                    "UnitsInStock": 13,
-                    "Discontinued": false,
-                },
-                {
-                    "ProductID": 4,
-                    "ProductName": "Chef Anton",
-                    "UnitPrice": 22,
-                    "UnitsInStock": 53,
-                    "Discontinued": false,
-                },
-                {
-                    "ProductID": 5,
-                    "ProductName": "Chef Gumbo Mix",
-                    "UnitPrice": 21.35,
-                    "UnitsInStock": 0,
-                    "Discontinued": true,
-                },
-                {
-                    "ProductID": 6,
-                    "ProductName": "Boysenberry Spread",
-                    "UnitPrice": 25,
-                    "UnitsInStock": 120,
-                    "Discontinued": false,
-                }]
+
+
+
+                myMovies: [{
+                    "movieName": 'Fight Club',
+                    "monthWatched": 1,
+                    "dayWatched": 5,
+                    "yearWatched": 1,
+                    "genre": 'thriller',
+                    "rating": 5,
+                    "comments": ''
+                }
+                ]
             }
         },
         methods: {
             togglePages: function (pageName) {
-                console.log('inside togglepages function', pageName)
                 if (pageName == 'home') {
                     this.home = true;
                 }
@@ -130,6 +133,31 @@ export default {
                     this.home = false;
                 }
                 return;
+            },
+            submitMovie: function () {
+                this.myMovies.push({
+                    "movieName": this.movieName,
+                    "monthWatched": this.monthWatched,
+                    "dayWatched": this.dayWatched,
+                    "yearWatched": this.yearWatched,
+                    "genre": this.genre,
+                    "rating": this.rating,
+                    "comments": this.comments
+                })
+            },
+            refreshForm: function () {
+                this.movieName = '',
+                this.monthWatched = 1,
+                this.dayWatched = 1,
+                this.yearWatched = 2019,
+                this.genre = '',
+                this.rating = 0,
+                this.comments = ''
+            },
+            submitForm: function () {
+                this.submitMovie();
+                this.togglePages('home');
+                this.refreshForm();
             }
         }
 
