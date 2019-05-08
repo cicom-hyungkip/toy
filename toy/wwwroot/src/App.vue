37e04a0362c498ade86ac2a4b8e4ef8bef5e0b30
Hyungki <hyungkip@cicomprogram.com>
2019-05-08 13:10:13 -0700

    [setup] adds kendo grids to dependency

--- toy/wwwroot/src/App.vue
+++ toy/wwwroot/src/App.vue
@@ -1,14 +1,65 @@
-﻿<template>
-    <div>
-        <h1>Hello World!</h1>
-    </div>
+﻿
+
+<template>
+    <kendo-grid :data-source="localDataSource">
+    </kendo-grid>
+    
 </template>
+
 <script>
 export default {
-    data: function () {
-        return {
-            greetings: 'hi'
+        /*data: function () {
+            return {
+                greetings: 'hi',
+                somebody: 'help'
+            }
+        }*/
+        data: function () {
+            return {
+                localDataSource: [{
+                    "ProductID": 1,
+                    "ProductName": "Chai",
+                    "UnitPrice": 18,
+                    "UnitsInStock": 39,
+                    "Discontinued": false,
+                },
+                {
+                    "ProductID": 2,
+                    "ProductName": "Chang",
+                    "UnitPrice": 17,
+                    "UnitsInStock": 40,
+                    "Discontinued": false,
+                },
+                {
+                    "ProductID": 3,
+                    "ProductName": "Aniseed Syrup",
+                    "UnitPrice": 10,
+                    "UnitsInStock": 13,
+                    "Discontinued": false,
+                },
+                {
+                    "ProductID": 4,
+                    "ProductName": "Chef Anton",
+                    "UnitPrice": 22,
+                    "UnitsInStock": 53,
+                    "Discontinued": false,
+                },
+                {
+                    "ProductID": 5,
+                    "ProductName": "Chef Gumbo Mix",
+                    "UnitPrice": 21.35,
+                    "UnitsInStock": 0,
+                    "Discontinued": true,
+                },
+                {
+                    "ProductID": 6,
+                    "ProductName": "Boysenberry Spread",
+                    "UnitPrice": 25,
+                    "UnitsInStock": 120,
+                    "Discontinued": false,
+                }]
+            }
         }
+
     }
-    }
-    </script>
+</script>
--- toy/wwwroot/src/app.js
+++ toy/wwwroot/src/app.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@progress/kendo-ui';
+import '@progress/kendo-theme-default/dist/all.css'
+
+import { Grid, GridInstaller } from '@progress/kendo-grid-vue-wrapper'
+
+Vue.use(GridInstaller);
+
 require('./lib')
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,11 +19,12 @@ import '../css/site.css'
 
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    components: {
+        Grid
+    }
 });
+
 
 document.getElementById("fillthis").innerHTML = getText();
 $('#fillthiswithjquery').html('Filled by Jquery!');
-
-let myES6Object = new ES6Lib();
-$('#fillthiswithes6lib').html(myES6Object.getData());
